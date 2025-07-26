621711e26569c3c7e7cf6f2cd8e4675bba1ffda3
Alessandro <jamancapiero85@gmail.com>
2025-07-26 18:24:17 -0300

    Finish

--- src/db.js
+++ src/db.js
@@ -6,7 +6,9 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    port:  process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false 
   }
 );
 
