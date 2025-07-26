7737e37feeb0cf3b691fe2991c420c0c996b0c34
Alessandro <jamancapiero85@gmail.com>
2025-07-26 19:36:52 -0300

    final

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({path: './src/.env'});
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port:  process.env.DB_PORT,
+    port:  process.env.dialect,
     dialect: "mysql",
     logging: false 
   }
